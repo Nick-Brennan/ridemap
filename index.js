82e9b6e3833d355d29f4d2d47664b9539fe16f0f
Nick Brennan <nick_p_b@yahoo.com>
2016-05-19 12:01:00 -0700

    setup express server to work with angular templates and added gmap

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+var path = require('path');
+var views = path.join(process.cwd(), "views");
+app.use("/static", express.static("public"));
+app.use("/vendor", express.static("bower_components"));
+
+app.get('/', function(req, res){
+  res.sendFile(views + '/index.html');
+});
+
+
+http.listen(process.env.PORT || 3000, function(){
+	console.log("RideMap is listening on port " + (process.env.PORT || 3000));
+});
